1bfa5e36295c9eee0f2a684443dce1c6c11cc893
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-04-02 19:50:50 +0330

    :hammer: chore: crate script for dark them

---
+++ change_background_dark_them.sh
@@ -0,0 +1,14 @@
+# a script to change background automatically in ubuntu
+
+
+export DISPLAY=:0
+export XDG_RUNTIME_DIR="/run/user/$(id -u)"
+
+# Set the directory containing your wallpapers
+DIR="$HOME/Pictures/Wallpapers/"
+
+# Select a random wallpaper from the directory
+PIC=$(ls $DIR/* | shuf -n1)
+
+# Set the selected image as the wallpaper
+gsettings set org.gnome.desktop.background picture-uri-dark "file://$PIC"
